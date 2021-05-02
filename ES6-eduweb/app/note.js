52b19574432001385a3c0ba24e6617ae383607da
Paweł Stech <pawelstech95@gmail.com>
2021-05-02 21:49:54 +0200

    template string

--- ES6-eduweb/app/note.js
+++ ES6-eduweb/app/note.js
@@ -500,3 +500,49 @@ const config = {
 };
 
 setSliderSpeed(config);
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// ---------------> 22. Template strings
+//
+
+let person = {
+  firstName: "Jan",
+  lastName: "Kowalski",
+  age: 49
+};
+
+let { firstName, lastName, age } = person;
+
+// let info = "Imię: " + firstName + ", nazwisko: " + lastName + ", wiek: " + age + " lat.";
+
+let info = `Imię: ${firstName}, nazwisko: ${lastName}, wiek: ${age} lat.`;
+
+console.log(info);
+
+let buttonText = "Wciśnij mnie!";
+
+// let template = "\
+// <button class='btn'>\
+//     <span>" + buttonText + "</span>\
+// <button>";
+
+let template = `
+<button class='btn'>
+  <span>${buttonText}</span>
+</button>
+`;
+
+console.log(template);
